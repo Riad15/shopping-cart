e4d75372e7c41c3c63b2f253c5903c4c54a54290
asif riad <asifriad15@gmail.com>
2022-02-15 02:52:12 +0600

    comlete project

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
 //------------------incrase-decrase btn function-------------------- 
-function numberOfCounter(numberOfInputId, isIncrse) {
-    const numberOfInput = document.getElementById(numberOfInputId);
-    const numberOfInputValue = parseInt(numberOfInput.value);
+function numberOfCounter(product, price, isIncrse) {
+    const numberOfInput = document.getElementById(product + '-number');
+    let numberOfInputValue = parseInt(numberOfInput.value);
     if (isIncrse) {
-        numberOfInput.value = numberOfInputValue + 1;
-    } else {
-        numberOfInput.value = numberOfInputValue - 1;
+        numberOfInputValue = numberOfInputValue + 1;
+
+    } else if (numberOfInputValue > 0) {
+        numberOfInputValue = numberOfInputValue - 1;
     }
+    numberOfInput.value = numberOfInputValue;
+    const totalAmount = document.getElementById(product + '-amount');
+    totalAmount.innerText = numberOfInputValue * price;
+    calcolateTotal();
 
 }
+function getInput(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calcolateTotal() {
+    const phoneTotal = getInput('iphone') * 1219;
+    const caseTotal = getInput('case') * 59;
+    const subtotal = phoneTotal + caseTotal;
+    const tax = subtotal / 10;
+    const totalPrice = subtotal + tax;
+
+
+    // update on the html
+    document.getElementById('sub-total').innerText = subtotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+
+// }
+
 //-----------------iphon number plus btn--------------------------------
 document.getElementById('iphone-incrase').addEventListener('click', function () {
-    numberOfCounter('iphone-number', true);
+    numberOfCounter('iphone', 1219, true);
 })
 
 //-----------------iphon number minous btn--------------------------------
 document.getElementById('inphone-decrase').addEventListener('click', function () {
-    numberOfCounter('iphone-number', false);
+    numberOfCounter('iphone', 1219, false);
 
 })
 
 //-----------------black case number minous btn--------------------------------
 document.getElementById('case-incrase').addEventListener('click', function () {
-    numberOfCounter('case-number', true);
+    numberOfCounter('case', 59, true);
 })
 
 //-----------------black case number minous btn--------------------------------
 document.getElementById('case-decrase').addEventListener('click', function () {
-    numberOfCounter('case-number', false);
+    numberOfCounter('case', 59, false);
 })
